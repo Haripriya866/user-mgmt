085948539d42cd9939a98dfa03f6230a09437a63
Haripriya866 <haripriyakanike5643@gmail.com>
2025-02-01 17:34:23 +0530

    Update server.js file in the backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -7,8 +8,57 @@ app.use(express.json());
 
 app.options("*", cors());
 
-const port = 5001;
+const port = 5000;
+
+app.get("/users", async (request, response) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    response.json(response.data);
+  } catch (error) {
+    response.status(500).json({ message: "Error fetching data" });
+  }
+});
+
+app.post("/users", async (request, response) => {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    response.json(response.data);
+  } catch (error) {
+    response.status(500).json({ message: "Error adding data" });
+  }
+});
+
+app.put("/users/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const updatedUser = request.body;
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`,updatedUser);
+    response.json(response.data); // Return updated user data
+  } catch (error) {
+    response.status(500).json({ message: "Error updating user" });
+  }
+});
+
+app.delete("/users/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const deletedUser = await axios.delete(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+
+    response.json({
+      message: "User deleted successfully",
+      data: deletedUser.data,
+    });
+  } catch (err) {
+    response.status(500).json({ message: "Error deleting user", error: err });
+  }
+});
+
 app.listen(port, () => {
-    console.log(`Backend server is running on http://localhost:${port}`);
-  });
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
+
   
